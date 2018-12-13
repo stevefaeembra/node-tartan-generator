2596d681b130e75b356491ecb07556a709d9be73
Steven Kay <stevendkay@gmail.com>
2018-12-13 22:30:52 +0000

    some refactoring

--- src/make_tartan.js
+++ src/make_tartan.js
@@ -1,20 +1,6 @@
 const fs = require('fs');
 const PNG = require('pngjs').PNG;
-
-// colour keys
-
-const palette = {
-  "K" : [0,0,0], //black
-  "B" : [0,0,255], //blue
-  "G" : [0,255,0], //green
-  "R" : [255,0,0], //red
-  "W" : [255,255,255], //white
-  "P" : [255,0,255], //purple
-  "Y" : [255,255,0], // yellow
-  "1" : [145,16,71],
-  "2" : [45,78,198],
-  "3" : [12,190,190]
-};
+const palette = require('./palette.js');
 
 // the sett defines the stitching pattern
 // array of arrays
@@ -37,6 +23,7 @@ const sett = [
 ];
 
 const setPixel = function (image,x,y,r,g,b) {
+  // set individual pixel of image
   var idx = (image.width * y + x) << 2;
   image.data[idx  ] = r;
   image.data[idx+1] = g;
@@ -62,29 +49,35 @@ const makeThreadSet = function (sett) {
   return result;
 }
 
-const threadCount = countThreads(sett);
-const threads = makeThreadSet(sett);
+const makeTartan = function (sett) {
 
-const im = new PNG({width:threadCount, height:threadCount, filterType: -1});
+  // make tartan from a sett description
 
-for (var y=0;y<im.height;y++) {
-  for (var x=0;x<im.width;x++) {
+  const threadCount = countThreads(sett);
+  const threads = makeThreadSet(sett);
+  const im = new PNG({width:threadCount, height:threadCount, filterType: -1});
 
-    const mx = x % 2;
-    const my = y % 2;
-    let threadColour = [];
+  for (var y=0;y<im.height;y++) {
+    for (var x=0;x<im.width;x++) {
 
-    if ((mx+my)%4 === 1) {
-      threadColour = palette[threads[x]];
-    } else {
-      threadColour = palette[threads[y]];
-    };
+      const mx = x % 2;
+      const my = y % 2;
+      let threadColour = [];
 
-    const r=threadColour[0];
-    const g=threadColour[1];
-    const b=threadColour[2];
-    setPixel(im,x,y,r,g,b);
-  }
-};
+      if ((mx+my)%4 === 1) {
+        threadColour = palette[threads[x]];
+      } else {
+        threadColour = palette[threads[y]];
+      };
 
-im.pack().pipe(fs.createWriteStream('newOut.png'));
+      const r=threadColour[0];
+      const g=threadColour[1];
+      const b=threadColour[2];
+      setPixel(im,x,y,r,g,b);
+    }
+  };
+  return im;
+}
+
+const myTartan = makeTartan(sett);
+myTartan.pack().pipe(fs.createWriteStream('newOut.png'));
---
+++ src/palette.js
@@ -0,0 +1,16 @@
+// thread colours
+
+const palette = {
+  "K" : [0,0,0], //black
+  "B" : [0,0,255], //blue
+  "G" : [0,255,0], //green
+  "R" : [255,0,0], //red
+  "W" : [255,255,255], //white
+  "P" : [255,0,255], //purple
+  "Y" : [255,255,0], // yellow
+  "1" : [145,16,71],
+  "2" : [45,78,198],
+  "3" : [12,190,190]
+};
+
+module.exports = palette;
