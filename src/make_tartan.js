70068285675f338bf12252f6c9b36fcdb0155e83
Steven Kay <stevendkay@gmail.com>
2018-12-13 22:45:44 +0000

    refactor into modules

--- src/make_tartan.js
+++ src/make_tartan.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const PNG = require('pngjs').PNG;
-const palette = require('./palette.js');
+const makeTartan = require('./weaver.js');
 
 // the sett defines the stitching pattern
 // array of arrays
@@ -22,62 +21,5 @@ const sett = [
   [3,"K"]
 ];
 
-const setPixel = function (image,x,y,r,g,b) {
-  // set individual pixel of image
-  var idx = (image.width * y + x) << 2;
-  image.data[idx  ] = r;
-  image.data[idx+1] = g;
-  image.data[idx+2] = b;
-  image.data[idx+3] = 255;
-};
-
-const countThreads = function (sett) {
-  // get total number of threads in sett
-  return sett.reduce((acc, item) => {
-    return acc += item[0];
-  },0);
-}
-
-const makeThreadSet = function (sett) {
-  // get thread set (a list of individual thread colour keys)
-  let result=[];
-  sett.forEach((item) => {
-    for (var t=0; t< item[0]; t++) {
-      result.push(item[1]);
-    }
-  });
-  return result;
-}
-
-const makeTartan = function (sett) {
-
-  // make tartan from a sett description
-
-  const threadCount = countThreads(sett);
-  const threads = makeThreadSet(sett);
-  const im = new PNG({width:threadCount, height:threadCount, filterType: -1});
-
-  for (var y=0;y<im.height;y++) {
-    for (var x=0;x<im.width;x++) {
-
-      const mx = x % 2;
-      const my = y % 2;
-      let threadColour = [];
-
-      if ((mx+my)%4 === 1) {
-        threadColour = palette[threads[x]];
-      } else {
-        threadColour = palette[threads[y]];
-      };
-
-      const r=threadColour[0];
-      const g=threadColour[1];
-      const b=threadColour[2];
-      setPixel(im,x,y,r,g,b);
-    }
-  };
-  return im;
-}
-
-const myTartan = makeTartan(sett);
+const myTartan = makeTartan(sett,3);
 myTartan.pack().pipe(fs.createWriteStream('newOut.png'));
---
+++ src/weaver.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const palette = require('./palette.js');
+
+const setPixel = function (image,x,y,r,g,b) {
+  // set individual pixel of image
+  var idx = (image.width * y + x) << 2;
+  image.data[idx] = r;
+  image.data[idx+1] = g;
+  image.data[idx+2] = b;
+  image.data[idx+3] = 255;
+};
+
+const countThreads = function (sett, scale=1) {
+  // get total number of threads in sett
+  return sett.reduce((acc, item) => {
+    return acc += item[0] * scale;
+  },0);
+}
+
+const makeThreadSet = function (sett, scale=1) {
+  // get thread set (a list of individual thread colour keys)
+  let result=[];
+  sett.forEach((item) => {
+    for (var t=0; t< item[0]*scale; t++) {
+      result.push(item[1]);
+    };
+  });
+  return result;
+}
+
+const makeTartan = function (sett, scale=1) {
+
+  // make tartan from a sett description and scale
+  // scale needs to be a power of 2 (e.g. 1=2, 2=4, 3=8 etc)
+  const scaleUp = 2**scale;
+  const threadCount = countThreads(sett, scaleUp);
+  const threads = makeThreadSet(sett, scaleUp);
+  const im = new PNG({width:threadCount, height:threadCount, filterType: -1});
+
+  for (var y=0;y<im.height;y++) {
+    for (var x=0;x<im.width;x++) {
+
+      const mx = x % 2;
+      const my = y % 2;
+      let threadColour = [];
+
+      if ((mx+my)%4 === 1) {
+        threadColour = palette[threads[x]];
+      } else {
+        threadColour = palette[threads[y]];
+      };
+
+      const r=threadColour[0];
+      const g=threadColour[1];
+      const b=threadColour[2];
+      setPixel(im,x,y,r,g,b);
+    }
+  };
+  return im;
+};
+
+module.exports = makeTartan;
